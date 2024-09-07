d2f6a67e7c96af9c7bb2670a4c9e1ac2e9f120d2
ByRoyhoVSA <Forest21@inbox.ru>
2024-09-07 23:59:59 +0300

    Update module_1_3#2.py

--- module_1_3#2.py
+++ module_1_3#2.py
@@ -2,7 +2,7 @@ name="Сергей"
 print("Name:",name)
 age=32
 print("Age:",age)
-new_age=13
-print("New Age:",age+new_age)
+age=age+13
+print("New Age:",age)
 is_student=True
 print("Is Student:",is_student)
